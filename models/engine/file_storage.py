c0007af5b69c62fc86d69769a33d6459debc181d
JennyHadir <hadirjenny@hotmail.com>
2021-04-03 22:43:54 +0100

    Fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -53,6 +53,5 @@ class FileStorage:
         """ Delet obj from __objects"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
                 del self.__objects[key]
-
+                self.save()
