d9c45cea83f9cd9419dd9f850e30ee6a60c8933a
JennyHadir <hadirjenny@hotmail.com>
2021-04-18 22:39:10 +0100

    Fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" abric script that generates a .tgz archive
+""" Fabric script that generates a .tgz archive
  from the contents of the web_static folder of
  my AirBnB Clone repo, using the function do_pack"""
 from datetime import datetime
@@ -8,6 +8,7 @@ import os.path
 
 
 def do_pack():
+    """ main function"""
     try:
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         file = "version/web_static_{}.tgz".format(date)
