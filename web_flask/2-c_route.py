a482f84c9983368f299c7cf5de348fba233dd3fa
JennyHadir <hadirjenny@hotmail.com>
2021-05-01 21:39:17 +0100

    ADD

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" Start Flask web app """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Return Hello hbnb """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """ Return C followed by the value of text"""
+    return 'C {}'.format(text.replace('_', ' '))
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
