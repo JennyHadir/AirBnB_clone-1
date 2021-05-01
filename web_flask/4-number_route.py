deac121bd667d6a826a6222f3be34036b203b397
JennyHadir <hadirjenny@hotmail.com>
2021-05-01 21:47:39 +0100

    FIX: file name

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" Start Flask web app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Return Hello hbnb """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hello():
+    """ Return HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C(text):
+    """ Return C followed by the value of text"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ Return python followed by text"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def numb(n):
+    """ Return n if it's a number"""
+    if isinstance(n, int):
+        return '{} is a number'.format(n)
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0', port='5000')
