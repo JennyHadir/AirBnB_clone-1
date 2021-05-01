d5c46d79900292ad07df48dc6a66e6889399bd16
JennyHadir <hadirjenny@hotmail.com>
2021-05-01 22:22:11 +0100

    Fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,8 +39,7 @@ def numb(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numb_template(n):
     """ Return a HTML page if n is a number"""
-    if isinstance(n, int):
-        return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.debug = True
